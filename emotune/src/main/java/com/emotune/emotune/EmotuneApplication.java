957902ba8b1a665616257ef11c23ca7b2592335c
Yash Pokharna <yashpokharna2512@gmail.com>
2025-05-17 13:22:33 -0700

    Initial Spring Boot backend setup

---
+++ emotune/src/main/java/com/emotune/emotune/EmotuneApplication.java
@@ -0,0 +1,13 @@
+package com.emotune.emotune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotuneApplication.class, args);
+	}
+
+}
